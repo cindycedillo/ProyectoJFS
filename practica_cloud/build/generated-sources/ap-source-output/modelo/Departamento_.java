00645866aa1710320ce06b18521e0ce77619c136
cindycedillo <90285103+cindycedillo@users.noreply.github.com>
2022-07-23 12:49:01 -0500

    Add files via upload

---
+++ practica_cloud/build/generated-sources/ap-source-output/modelo/Departamento_.java
@@ -0,0 +1,17 @@
+package modelo;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import modelo.Empleado;
+
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2022-07-23T11:42:56")
+@StaticMetamodel(Departamento.class)
+public class Departamento_ { 
+
+    public static volatile SingularAttribute<Departamento, Integer> idDepartamento;
+    public static volatile CollectionAttribute<Departamento, Empleado> empleadoCollection;
+    public static volatile SingularAttribute<Departamento, String> nombre;
+
+}
